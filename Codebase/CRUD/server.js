d8c68f4dd406b18e2e606647192e55745c900ae7
Nipun Ayodya <it21187278@my.sliit.lk>
2022-12-27 19:31:50 +0530

    id

--- Codebase/CRUD/server.js
+++ Codebase/CRUD/server.js
@@ -4,9 +4,18 @@ const app = express()
 app.get('/read-users' , (req,res)=>{
 res.send("Users is here");
 
+
+});
+
+app.get('/get-user-details',(req,res)=>{
+    const iserOBJ={
+        id:10,
+        nam:"KAMAL",
+        lastName:"JJR",
+        status:true
+    }
 });
 
 
-app.get()
 
 app.listen(3001);
